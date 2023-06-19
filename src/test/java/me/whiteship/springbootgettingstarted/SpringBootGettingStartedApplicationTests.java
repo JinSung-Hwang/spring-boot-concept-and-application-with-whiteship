fafbd507f2370bfb2ec9df09454e9daec7c67e90
JinSung-Hwang <wlstjd7782@gmail.com>
2023-06-20 00:17:12 +0900

    init: project initial code

---
+++ src/test/java/me/whiteship/springbootgettingstarted/SpringBootGettingStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.springbootgettingstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGettingStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
